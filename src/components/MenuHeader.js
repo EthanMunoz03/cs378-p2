78ecd9eb6b99826004ca6fa3f2e2ccccbb4d9502
Ethan03 <notrealethan@gmail.com>
2025-02-07 18:28:35 -0600

    FINALLY

--- src/components/MenuHeader.js
+++ src/components/MenuHeader.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function MenuHeader() {
     return (
-        <div className="menu-header">
-            <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Bevo Bento Logo" className="menu-logo" />
-            <h1 className="menu-title">Miso Bevo</h1>
+        <div>
+            <div className="menu-header">
+                <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Bevo Bento Logo" className="menu-logo" />
+                <h1 className="menu-title">Miso Bevo</h1>
+            </div>
             <p className="menu-tagline">Where Bevo Meets Bento</p>
         </div>
     );
